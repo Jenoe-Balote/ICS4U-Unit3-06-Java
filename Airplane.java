74494adc4b5fa4b76619f3c001bcadd8d9c24662
Ubuntu <ubuntu@ip-172-31-94-200.ec2.internal>
2022-01-04 17:39:57 +0000

    Added *.java

---
+++ Airplane.java
@@ -0,0 +1,43 @@
+/*
+ * This is an Airplane class.
+ *
+ * @author  Jenoe Balote
+ * @version 1.0
+ * @since   2021-12-31
+ */
+
+/**
+ * This is the Airplane class.
+ * */
+public class Airplane {
+    /**
+     * The speed of the airplane.
+     * */
+    private int speed;
+
+    /**
+     * Initialization.
+     * */
+    public Airplane() {
+        this.speed = 0;
+    }
+
+    /**
+     * Getter for speed.
+     *
+     * @return speed
+     * */
+    public int getSpeed() {
+        return speed;
+    }
+
+    /**
+     * Setter for speed.
+     *
+     * @param newSpeed new speed
+     * */
+    public void setSpeed(final int newSpeed) {
+        this.speed = newSpeed;
+    }
+}
+
---
+++ Jet.java
@@ -0,0 +1,34 @@
+/*
+ * This is a Jet class.
+ *
+ * @author  Jenoe Balote
+ * @version 1.0
+ * @since   2021-12-31
+ */
+
+/**
+ * Jet class based off of Airplane.
+ * */
+public class Jet extends Airplane {
+
+    /**
+     * Speed multiplier.
+     * */
+    private static final int MULTIPLIER = 2;
+
+    /**
+     * Sets the jet's speed.
+     *
+     * @param speed the new speed
+     * */
+    public void setSpeed(final int speed) {
+        super.setSpeed(speed * MULTIPLIER);
+    }
+
+    /**
+     * Accelerates the jet's speed.
+     * */
+    public void accelerate() {
+        super.setSpeed(getSpeed() * MULTIPLIER);
+    }
+}
